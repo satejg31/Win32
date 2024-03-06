6a4a0a60d97cf2ed0e5583c5c8331c88f0e5e070
satejg31 <satej.ganji@gmail.com>
2024-03-06 21:14:06 +0530

    Initial commit

---
+++ 02-MessageBox/MyMessageWindow.cpp
@@ -0,0 +1,84 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(szAppName,
+			TEXT("SUG:MyFirstWindow-MessageBox"),
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+	
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+	
+	while(GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return ((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	
+	switch(iMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		case WM_CREATE:
+			MessageBox(hwnd, TEXT("Window is created"), TEXT("Welcome"), MB_OK);
+			break;
+		
+		case WM_LBUTTONDOWN:
+			MessageBox(hwnd, TEXT("Left button is pressed"), TEXT("Left Button"), MB_OK);
+			break;
+
+		case WM_RBUTTONDOWN:
+			MessageBox(hwnd, TEXT("Right button is pressed"), TEXT("Right Button"), MB_OK);
+			break;
+
+		case WM_KEYDOWN:
+			MessageBox(hwnd, TEXT("Key is pressed"), TEXT("Key Down"), MB_OK);
+			break;
+
+		default:
+			break;
+	}
+
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+
+}
