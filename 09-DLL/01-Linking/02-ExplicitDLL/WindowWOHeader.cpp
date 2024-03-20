a8d4641f605c82af4cca268ef47980224130d712
satejg31 <satej.ganji@gmail.com>
2024-03-20 23:20:26 +0530

    Initial commit

---
+++ 09-DLL/01-Linking/02-ExplicitDLL/WindowWOHeader.cpp
@@ -0,0 +1,106 @@
+#include <windows.h>
+
+//Global Function Declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Global Variable Declaraions
+
+//Entry Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//Local Variable Declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+
+	//code
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	//handle
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	RegisterClassEx(&wndclass);
+
+	//Create Window in memory
+	hwnd = CreateWindow(szAppName,
+		TEXT("SUG : MyFirstWindow"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	//Display Window
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//Message Loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//local variables
+	HMODULE hLib = NULL;
+	typedef int (*PMAKESQUAREFUNCTION)(int);
+	PMAKESQUAREFUNCTION pMakeSquare = NULL;
+	int num1, num2;
+	TCHAR str[255];
+
+	//code
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		hLib = LoadLibrary(TEXT("MyMath.dll"));
+		if (hLib == NULL)
+		{
+			MessageBox(hwnd, TEXT("LoadLibrary Failed"), TEXT("Error"), MB_OK);
+			DestroyWindow(hwnd);
+		}
+		pMakeSquare = (PMAKESQUAREFUNCTION)GetProcAddress(hLib, "MakeSquare");
+		
+		if (pMakeSquare == NULL)
+		{
+			MessageBox(hwnd, TEXT("Make Square Function address cannot be obtained"), TEXT("Error"), MB_OK);
+			DestroyWindow(hwnd);
+		}
+		num1 = 15;
+		num2 = pMakeSquare(num1);
+		wsprintf(str, TEXT("The square of %d is %d"), num1, num2);
+		MessageBox(hwnd, str, TEXT("Square"), MB_OK);
+		FreeLibrary(hLib);
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
